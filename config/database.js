cd2545f6ced969230c0af6dc0a33546ec0caf744
Salma Khan <hussainsalma37@gmail.com>
2020-01-17 13:10:37 -0600

    creates database for the project

---
+++ config/database.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.DATABASE_URL, { 
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+});
+
+const db = mongoose.connection;
+
+db.once('connected', () => {
+    console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+});
--- server.js
+++ server.js
@@ -5,12 +5,18 @@ var favicon = require('serve-favicon');
 
 var app = express();
 
+// connecting to database
+require('dotenv').config();
+require('./config/database');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 //api routes must be before the "catch all" route
+
+
 
 //the "catch all" route
 app.get('/*', function(req, res) {
