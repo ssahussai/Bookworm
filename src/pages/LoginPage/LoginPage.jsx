1a0f0d247d5af516783d0f7177230bdf7caaabad
Salma Khan <hussainsalma37@gmail.com>
2020-01-23 13:06:33 -0600

    styled log in page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -27,22 +27,22 @@ class LoginPage extends Component {
 
   render() {
     return (
-      <div>
-        <header>Log In</header>
-        <form onSubmit={this.handleSubmit} >
-          <div>
-            <div>
-              <input type="email" placeholder="Email" value={this.state.email} name="email" onChange={this.handleChange} />
+      <div className="LoginPage">
+        <header className="header-footer">Log In</header>
+        <form className="form-horizontal" onSubmit={this.handleSubmit} >
+          <div className="form-group">
+            <div className="col-sm-12">
+              <input type="email" className="form-control" placeholder="Email" value={this.state.email} name="email" onChange={this.handleChange} />
             </div>
           </div>
-          <div>
-            <div>
-              <input type="password" placeholder="Password" value={this.state.pw} name="pw" onChange={this.handleChange} />
+          <div className="form-group">
+            <div className="col-sm-12">
+              <input type="password" className="form-control" placeholder="Password" value={this.state.pw} name="pw" onChange={this.handleChange} />
             </div>
           </div>
-          <div>
-            <div>
-              <button>Log In</button>
+          <div className="form-group">
+            <div className="col-sm-12 text-center">
+              <button className="btn btn-default">Log In</button>&nbsp;&nbsp;&nbsp;
               <Link to='/'>Cancel</Link>
             </div>
           </div>
